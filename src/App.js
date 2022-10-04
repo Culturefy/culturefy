b4510cd0471c070991391fffc0cb80bd3c66facd
TitoJerson <114895595+TitoJerson@users.noreply.github.com>
2022-10-04 11:47:28 +0530

    src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { Route, Routes } from 'react-router-dom';
+import Deshboard from './Dashboard/Index.js'
+import './App.css';
+
+function App() {
+  return (
+   <>
+   <Deshboard/>
+   </>
+  );
+}
+
+export default App;
