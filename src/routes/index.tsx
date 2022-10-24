242128831ab83ed74632345e4f20effb421e22ba
Haris Ghori <haris.ghori@binaryit.co>
2022-10-24 20:52:22 +0500

    ready for deployment

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -70,7 +70,7 @@ export const router = createBrowserRouter([
     element: <UserLayout />,
     children: [
       {
-        path: "brand-culture-startegy",
+        path: "/",
         element: <BrandCultureStartegy />,
         
       },
